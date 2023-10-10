e542ce527dfad64990b0680841ad9b0107dfd69c
HugoDPSz <128089628+HugoDPSz@users.noreply.github.com>
2023-10-10 11:33:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include "leitura.h"
 
 int main(void){
-    char comando[100000][100015]; //Espaço necessário para o comando mais longo
+    char comando[100][115]; //Espaço necessário para o comando mais longo
     int x, i;
-    scanf("%d", &x); //Para saber quantas linhas serão usadas
+    scanf("%d\n", &x); //Para saber quantas linhas serão usadas
     
     for(i=0; i<x; i++){
         leComando(comando, i);
