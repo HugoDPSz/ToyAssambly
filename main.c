2d058cf2f882057ae5d5ec71f7375855c34bc8f5
HugoDPSz <128089628+HugoDPSz@users.noreply.github.com>
2023-10-23 21:22:45 -0300

    23/10 main.c

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 #include "leitura.h"
+#include "controle.h"
 
 int main(void){
-    char comando[100][115]; //Espaço necessário para o comando mais longo
+    char comando[100][30]; //Espaço necessário para o comando mais longo
     int x, i;
     scanf("%d\n", &x); //Para saber quantas linhas serão usadas
     
     for(i=0; i<x; i++){
-        leComando(comando, i);
+        leComando(comando, i); //Lendo as isntrucoes
     }
+
+    controle(comando); //Aqui onde o trabalho acontece
 
     return 0;
 }
